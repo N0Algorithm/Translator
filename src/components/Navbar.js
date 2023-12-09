cee2c447162c14434f5b97cf470351604c53394d
N0Algorithm <akshay312004@gmail.com>
2023-12-09 12:53:03 +0530

    Darkmode added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,13 @@ import { useState } from 'react';
 const Navbar = (props) => {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [dmode, setdMode] = useState('Enable DarkMode'); // Whether dark mode is enabled or not
+  // const [name, setName] = useState('Translator'); // Whether dark mode is enabled or not
   
   const click1 =()=>{
     if(mode === 'light' && dmode==='Enable DarkMode' ){
       setMode('dark');
       setdMode('Enable LightMode');
-      
+      document.getElementById("t1").style.color = "white";
       document.body.style.backgroundColor = '#212024';
       document.getElementById("fromcpy").style.color = "white";
       document.getElementById('fromcpy').style.backgroundColor='#161716'
@@ -20,6 +21,8 @@ const Navbar = (props) => {
       setMode('light');
       setdMode('Enable DarkMode');
       document.body.style.backgroundColor = '#f5f5f5';
+      document.getElementById("t1").style.color = "black";
+      
       document.getElementById("fromcpy").style.color = "black";
       document.getElementById('fromcpy').style.backgroundColor='#FFFFFF'
       document.getElementById('tocpy').style.color='black';
@@ -33,7 +36,7 @@ const Navbar = (props) => {
     
   <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Translator</a>
+    <a class="navbar-brand" id="t1" href="/">Translator</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
